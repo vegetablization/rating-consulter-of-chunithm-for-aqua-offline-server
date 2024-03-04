0a463fa9a2c4428ab625adc10b5e5c1dbd0b86c3
vegetablization <1174840437@qq.com>
2024-03-05 03:16:36 +0800

    -

--- cha.py
+++ cha.py
@@ -156,16 +156,5 @@ if __name__ == "__main__":
 
     cur.close()
     conn.close()
-# rating-consulter-for-aqua-offline-server
-# 自己写着用的中二Aqua离线服查分器，所有版本都能用（应该）
 
-# 在命令行输
-
-# python path\to\file\cha.py 
-
-# 就能用
-
-# 如果加了新的opt或者版本更新了，可以直接更新歌曲定数
-
-# python path\to\file\cha.py -u
 
